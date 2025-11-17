6a6aec11af9198917158c87d4b2e2172b4dec87e
Abhishek kumar <india52736628@gmail.com>
2025-11-17 20:51:27 +0530

    todoApi

---
+++ src/main/java/com/example/TodoApi/Todo.java
@@ -0,0 +1,26 @@
+package com.example.TodoApi;
+
+import java.util.*;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Todo {
+        private static List<Response> todoList;
+        public Todo(){
+            todoList= new ArrayList<>();
+            todoList.add(new Response(1, false, "gym", 11));
+            todoList.add(new Response(2, false, "reading", 12));
+            todoList.add(new Response(3, false, "writting", 13));
+
+        }
+
+        @GetMapping("/todoList")
+        public List<Response> getTodo(){
+            return todoList;
+        }
+
+
+
+}
